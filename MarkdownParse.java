6b6b96d35ce336b8cde2e0a14fae78d11054e9c4
Scott Bell <59124418+LippsVega@users.noreply.github.com>
2022-01-28 11:31:33 -0800

    removed fixes
    removed fixes to show them sequentially in lab report

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,20 +12,9 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            if (nextOpenBracket == -1) {
-                return toReturn;
-            }
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
-            if (openParen == -1) {
-                currentIndex = nextCloseBracket + 1;
-                continue;
-            }
             int closeParen = markdown.indexOf(")", openParen);
-            if (nextOpenBracket != 0 && markdown.charAt(nextOpenBracket - 1) == '!') {
-                currentIndex = closeParen + 1;
-                continue;
-            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
