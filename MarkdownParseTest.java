ca57a302765ff5886ef9ec9025e49f18525ef33e
Scott Bell <59124418+LippsVega@users.noreply.github.com>
2022-01-27 16:36:54 -0800

    added junit tests
    added test for image,
    test for firstline,
    test for format

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -13,10 +13,34 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void testNewFile() throws IOException {
+    public void testFile() throws IOException {
         Path fileName = Path.of("test-file.md");
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         assertEquals(List.of("https://something.com", "some-page.html"), links);
+    }
+
+    @Test
+    public void testImageFile() throws IOException {
+        Path fileName = Path.of("image-file.md");
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of("https://something.com"), links);
+    }
+
+    @Test
+    public void testFirstLine() throws IOException {
+        Path fileName = Path.of("firstline.md");
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of("https://something.com"), links);
+    }
+
+    @Test
+    public void testFormat() throws IOException {
+        Path fileName = Path.of("format.md");
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of(), links);
     }
 }
