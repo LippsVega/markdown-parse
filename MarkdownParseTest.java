750242f749c79fc2b7e67a9daa3913b5eefceef2
Scott Bell <59124418+LippsVega@users.noreply.github.com>
2022-02-03 15:52:59 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class MarkdownParseTest {
     @Test
     public void addition() {
-        assertNotEquals(2, 1 + 1);
+        assertEquals(2, 1 + 1);
     }
 
     @Test
